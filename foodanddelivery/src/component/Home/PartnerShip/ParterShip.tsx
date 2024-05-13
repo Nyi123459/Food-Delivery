269eece34c44bc1298bd05417018dc91e10fa545
Thandar-Lin11 <thandarlinttu@gmail.com>
2024-05-13 09:17:56 +0630

    Changes

--- foodanddelivery/src/component/Home/PartnerShip/ParterShip.tsx
+++ foodanddelivery/src/component/Home/PartnerShip/ParterShip.tsx
@@ -21,35 +21,41 @@
 
 // export default ParterShip;
 
-import { partnerShips } from "../../../content";
+// import { partnerShips } from "../../../content";
 
-const ParterShip = () => {
-  return (
-    <section className="container mx-auto px-4 md:px-8 lg:px-16 xl:px-20 py-8 pt-6">
-      <div className="flex flex-wrap justify-center gap-8">
-        {partnerShips.map((data, index) => (
-          <div className="flex flex-col items-center max-w-xs" key={index}>
-            <img
-              src={data.icon}
-              alt="icon"
-              className="w-40 h-40 md:w-40 md:h-40 mb-2"
-            />
-            <div className="text-center pt-4">
-              <h3 className="text-lg md:text-2xl font-semibold mb-1">
-                {data.title}
-              </h3>
-              <p className="text-sm md:text-base mb-2 pt-4">{data.text}</p>
-              <a
-                href={data.link}
-                className="text-red-600 text-base font-bold md:text-base pt-4">
-                {data.link}
-              </a>
-            </div>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
-};
+// const ParterShip = () => {
+//   return (
+//     <section className="container mx-auto px-4 md:px-8 lg:px-16 xl:px-20 py-8 pt-6">
+//       <div className="flex flex-wrap justify-center gap-8">
+//         {partnerShips.map((data, index) => (
+//           <div
+//             className="flex flex-col items-center max-w-sm md:max-w-xs lg:max-w-sm"
+//             key={index}
+//           >
+//             <img
+//               src={data.icon}
+//               alt="icon"
+//               className="w-32 h-32 md:w-40 md:h-40 lg:w-32 lg:h-32 mb-2"
+//             />
+//             <div className="text-center pt-4">
+//               <h3 className="text-lg md:text-2xl font-semibold mb-1">
+//                 {data.title}
+//               </h3>
+//               <p className="text-sm md:text-base mb-2 pt-2 md:pt-4">
+//                 {data.text}
+//               </p>
+//               <a
+//                 href={data.link}
+//                 className="text-red-600 text-base font-bold md:text-base pt-2 md:pt-4"
+//               >
+//                 {data.link}
+//               </a>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </section>
+//   );
+// };
 
-export default ParterShip;
+// export default ParterShip;
